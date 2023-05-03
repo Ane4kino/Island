3995892f7c6dfa9330faefaccdf8cf8e75301726
Anna Skripkina <skripwomen@gmail.com>
2023-05-03 14:59:05 +0200

    3-05-23

--- src/com/island/Main.java
+++ src/com/island/Main.java
@@ -13,32 +13,41 @@ public class Main {
 
         BaseEntityPopulation population = new BaseEntityPopulation();
         AnimalFactory animalFactory = new AnimalFactory(new ObjectMapper(), population);
-
         AnimalGrid animalGrid = new AnimalGrid(population);
-        Cell[][] table = animalGrid.getTable();
         animalGrid.populateTable(animalFactory, population);
-
-
-//        animalGrid.printAllGrid(population);
+        Cell[][] table = animalGrid.getTable();
         Actions actions = new Actions(table);
-        System.out.println("Первый запуск животных");
-        animalGrid.printGrid(population);
-        System.out.println();
-        System.out.println("*********************");
-        for (int i = 0; i < 20; i++) {
-            actions.move(table,population);
-            actions.eating(population);
-            actions.performBreeding(animalFactory, population);
-            actions.runningTime(population);
-            animalGrid.printGrid(population);
-            System.out.println();
-            System.out.println("*********************");
-//            animalGrid.printAllGrid(population); //будет выводить в консоль в виде таблицы
 
+        Runner runner = new Runner(actions, animalFactory, table, population,animalGrid);
+        runner.start();
+//        BaseEntityPopulation population = new BaseEntityPopulation();
+//        AnimalFactory animalFactory = new AnimalFactory(new ObjectMapper(), population);
+//
+//        AnimalGrid animalGrid = new AnimalGrid(population);
+//        Cell[][] table = animalGrid.getTable();
+//        animalGrid.populateTable(animalFactory, population);
+//
+//
+////        animalGrid.printAllGrid(population);
+//        Actions actions = new Actions(table);
+//        System.out.println("Первый запуск животных");
+//        animalGrid.printGrid(population);
+//        System.out.println();
+//        System.out.println("*********************");
+//        for (int i = 0; i < 20; i++) {
+//            actions.move(table,population);
+//            actions.eating(population);
+//            actions.performBreeding(animalFactory, population);
+//            actions.runningTime(population);
 //            animalGrid.printGrid(population);
-//            animalGrid.printAllGrid(population);
-
-        }
+//            System.out.println();
+//            System.out.println("*********************");
+////            animalGrid.printAllGrid(population); //будет выводить в консоль в виде таблицы
+//
+////            animalGrid.printGrid(population);
+////            animalGrid.printAllGrid(population);
+//
+//        }
 
 
     }
--- src/com/island/Runner.java
+++ src/com/island/Runner.java
@@ -1,17 +1,48 @@
 package com.island;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.island.BaseEntity.BaseEntity;
+
 import com.island.frame.AnimalGrid;
 import com.island.frame.Cell;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class Runner implements Runnable{
+public class Runner extends Thread {
+    private Actions actions;
+    private BaseEntityPopulation population;
+    private AnimalFactory animalFactory;
+    private Cell[][] table;
+    private AnimalGrid animalGrid;
 
+    public Runner(Actions actions, AnimalFactory animalFactory, Cell[][] table, BaseEntityPopulation population, AnimalGrid animalGrid) throws IOException {
+        this.actions = actions;
+        this.animalFactory = animalFactory;
+        this.table = table;
+        this.population = population;
+        this.animalGrid = animalGrid;
+    }
 
     @Override
     public void run() {
-        Thread thread=Thread.currentThread();
 
+        System.out.println("Первый запуск животных");
+        animalGrid.printGrid(population);
+        System.out.println();
+        System.out.println("*********************");
+        ExecutorService service = Executors.newFixedThreadPool(3);
+        for (int i = 0; i < 20; i++) {
+            actions.move(table, population);
+            actions.eating(population);
+            try {
+                actions.performBreeding(animalFactory, population);
+            } catch (IOException | InstantiationException | IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+            actions.runningTime(population);
+            animalGrid.printGrid(population);
+            System.out.println();
+            System.out.println("*********************");
+        }
+        service.shutdown();
     }
 }
